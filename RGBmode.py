5bcfd6fcdabde0f77031d5e60b92c2c56bea68d6
Yury Namgung <yury.namgung@gmail.com>
2018-07-01 21:27:59 -0700

    Create RGBmode.py
    A CNN to classify large scenes from LSUN dataset in practice for classifying RGB error images from Spectral Optics analysis program

---
+++ RGBmode.py
@@ -0,0 +1,6 @@
+# this CNN pulls from other models i.e. yunjey's which IDs numbers on MNIST dataset
+# it classifies the LSUN dataset that uses RGB images: classification of large scenes
+# There are 10 categories: bedroom, bridge, church outdoor, classroom, conference room, dining room, kitchen, living room, restaurant, tower
+
+import torch 
+import
